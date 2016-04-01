694050f2e394866ffa7b583c7716ddd763416748
Eric Rudisill <erudisill@cphandheld.com>
2016-04-01 11:32:37 -0700

    added UnisonAPI

--- Assets/Scripts/ZoneTrigger.cs
+++ Assets/Scripts/ZoneTrigger.cs
@@ -5,15 +5,18 @@ public class ZoneTrigger : MonoBehaviour {
 
 	private Light myLight;
 	private MeshRenderer myText;
-	 
+    private UnisonAPI api;
+
 	void Awake() {
 		myLight = GetComponentInChildren<Light> ();
 		myText = GetComponentInChildren<MeshRenderer> ();
+        api = GetComponent<UnisonAPI>();
 	}
 
 	void OnTriggerEnter(Collider other) {
 		myLight.enabled = true;
 		myText.enabled = true;
+        api.MoveInventory();
 	}
 
 	void OnTriggerExit(Collider other) {
---
+++ Assets/UnisonAPI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnisonAPI : MonoBehaviour {
+
+    public int BinId;
+
+    public void MoveInventory() {
+        StartCoroutine(DoMoveInventory());
+    }
+
+    private IEnumerator DoMoveInventory() {
+        // Create a Web Form
+        WWWForm form = new WWWForm();
+        form.AddField("VIN", "SCBFH7ZA1GC054247");
+        form.AddField("BinId", BinId);
+
+        // Upload to a cgi script
+        WWW w = new WWW("http://unison-dev.cphandheld.com/api/Inventory/NadaMoveInventory", form);
+        yield return w;
+        if (!string.IsNullOrEmpty(w.error)) {
+            print(w.error);
+        }
+        else {
+            print("Moved to bin " + BinId);
+        }        
+    }
+}
