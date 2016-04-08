091e99cc02fb2a27f8e84dfa91c3acb20f86ad1d
Eric Rudisill <erudisill@cphandheld.com>
2016-04-08 10:06:03 -0500

    added office

---
+++ Assets/UnisonRestApi.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnisonRestApi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void TestSubmitClick() {
+		StartCoroutine (TestSubmit());
+	}
+
+	IEnumerator TestSubmit() {
+		string url = "http://unison-dev.cphandheld.com/api/Inventory/NadaMoveInventory";
+
+		WWWForm form = new WWWForm ();
+		form.headers.Add ("Content-Type", "application/x-www-form-urlencoded");
+		form.AddField("VIN", "2HKRM4H7XEH679791");
+		form.AddField("BinId", 2);
+
+		WWW w = new WWW(url, form);
+		yield return w;
+
+		if (!string.IsNullOrEmpty (w.error)) {
+			print ("NadaMoveInventory fail ... " + w.error);
+		} else {
+			print ("NadaMoveInventory SUCCESS!");
+		}
+
+	}
+
+}
