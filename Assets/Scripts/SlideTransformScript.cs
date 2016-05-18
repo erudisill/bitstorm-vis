f71e6f68f4d20898c5a44d72ab44b307c5a1fb5b
Eric Rudisill <erudisill@cphandheld.com>
2016-05-18 08:40:11 -0500

    added zoom slider

---
+++ Assets/Scripts/SlideTransformScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SlideTransformScript : MonoBehaviour {
+
+    public bool SlideX = false;
+    public bool SlideY = false;
+    public bool SlideZ = false;
+    public GameObject Target;
+    public float Factor = 5f;
+
+    private Vector3 startPos;
+
+    public void Start() {
+        startPos = Target.transform.position;
+    }
+
+    public void Slide(Slider slider) {
+        float d = Factor * slider.value;
+        Vector3 pos = startPos;
+        if (SlideX) pos.x += d;
+        if (SlideY) pos.y += d;
+        if (SlideZ) pos.z += d;
+        //TODO: Lerp this
+        Target.transform.position = pos;
+    }
+}
