ea23c99941c06eae562414b552d4617788b3dfa2
Eric Rudisill <erudisill@cphandheld.com>
2016-05-25 17:21:27 -0500

    added simplewebgl

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate (new Vector3 (0, Time.deltaTime * Mathf.Rad2Deg, 0));
+	}
+}
